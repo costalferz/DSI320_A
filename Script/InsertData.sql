f47e381b85da036590215078dec169834f781a27
costalferz <costalferzus@gmail.com>
2023-04-13 00:43:30 +0700

    first commit

---
+++ Script/InsertData.sql
@@ -0,0 +1,7 @@
+INSERT INTO Music (music_id, title, artist, genre, duration)
+VALUES
+  (1, 'Bohemian Rhapsody', 'Queen', 'Rock', '00:05:54'),
+  (2, 'Stairway to Heaven', 'Led Zeppelin', 'Rock', '00:08:03'),
+  (3, 'Imagine', 'John Lennon', 'Pop', '00:03:04'),
+  (4, 'Billie Jean', 'Michael Jackson', 'Pop', '00:04:54'),
+  (5, 'Smells Like Teen Spirit', 'Nirvana', 'Grunge', '00:05:01');
